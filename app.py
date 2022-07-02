a3acf73ac16e706ec5d6aabf728d09ed50704197
Cherepanov Andrey <cherepanov.andrey944@gmail.com>
2022-07-02 17:04:39 +0500

    add server logic

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask
 from flask import render_template
+from game_of_life import GameOfLife
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    GameOfLife(width=25, height=25)
     return render_template("index.html")
 
 @app.route("/live")
 def live():
-    return render_template("live.html")
+    life = GameOfLife()
+    life.form_new_generation();
+    return render_template("live.html",
+                            life=life)
 
 if __name__ == "__main__":
     app.run('0.0.0.0', port=5000)
