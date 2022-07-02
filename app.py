4275557ee85163344fdb5279f9db4955129d56d9
Cherepanov Andrey <cherepanov.andrey944@gmail.com>
2022-07-02 16:30:23 +0500

    add route fuctions

--- app.py
+++ app.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/live")
+def live():
+    return render_template("live.html")
+
 if __name__ == "__main__":
     app.run('0.0.0.0', port=5000)
